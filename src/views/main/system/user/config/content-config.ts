3566642e2a41bca02a7623284068fc27eef76826
Lenny <2484579947@qq.com>
2024-03-10 22:04:08 +0800

    page-search completed

---
+++ src/views/main/system/user/config/content-config.ts
@@ -0,0 +1,42 @@
+const contentConfig = {
+  tableItems: [
+    {
+      type: "selection",
+      width: "60"
+    },
+    {
+      type: "index",
+      width: "60",
+      label: "序号"
+    },
+    {
+      type: "normal",
+      width: "150",
+      prop: "name",
+      label: "用户名"
+    },
+    {
+      type: "normal",
+      width: "150",
+      prop: "realname",
+      label: "真实姓名"
+    },
+    {
+      type: "normal",
+      width: "150",
+      prop: "cellphone",
+      label: "手机号码"
+    },
+    {
+      type: "slot",
+      width: "100",
+      prop: "enable",
+      label: "手机号码"
+    },
+    {
+      
+    }
+  ]
+};
+
+export default contentConfig;
